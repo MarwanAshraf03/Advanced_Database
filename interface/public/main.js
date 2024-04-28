7ecf647ebab1333aacfcd06cbf2eb4ab85b335f5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-04-28 22:41:50 +0300

    patient cont'd

--- interface/public/main.js
+++ interface/public/main.js
@@ -31,3 +31,26 @@
 	    })
 	    .catch(error => console.error('Error fetching patient data:', error));
   };
+ document.getElementById("add").onclick = function () {
+	// let formDiv = document.getElementById("add-form");
+	// document.getElementsByClassName("add-form").style.display = "flex";
+	document.querySelector(".add-form").style.display = "flex";
+	// document.querySelector(".add-form").addEventListener('submit', function () {
+	// 	let name = document.getElementById("name").value;
+	// 	let address = document.getElementById("address").value;
+	// 	let birth_date = document.getElementById("birth_date").value;
+	// 	let ph_number = document.getElementById("ph_number").value;
+	// 	let number_of_days = document.getElementById("number_of_days").value;
+	// 	let room_id = document.getElementById("room_id").value;
+	// 	document.querySelector(".add-form").style.display = "none";
+	//  });
+	document.getElementById("input-submit-add").onclick = function () {
+			let name = document.getElementById("name").value;
+			let address = document.getElementById("address").value;
+			let birth_date = document.getElementById("birth_date").value;
+			let ph_number = document.getElementById("ph_number").value;
+			let number_of_days = document.getElementById("number_of_days").value;
+			let room_id = document.getElementById("room_id").value;
+			document.querySelector(".add-form").style.display = "none";
+	}
+};
