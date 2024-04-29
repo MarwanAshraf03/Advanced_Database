ec329d4382a29bad4ef5bffbcf929660c030cedd
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-04-29 08:12:15 +0300

    index done

--- interface/public/main.js
+++ interface/public/main.js
@@ -1,49 +1,43 @@
-// document.getElementById("list-btn").onclick = function() {
-// 	// Fetch patient data from the server
-// 	document.getElementById("patientList").innerHTML = "";
-// 	fetch('/patients')
-// 	    .then(response => response.json())
-// 	    .then(data => {
-// 		  // Iterate through the patient data and display it
-// 		  data.forEach(patient => {
-// 			const patientList = document.getElementById('patientList');
-// 			const patientItem = document.createElement('p');
-// 			patientItem.textContent = `Name: ${patient.name}, Address: ${patient.address}`;
-// 			patientList.appendChild(patientItem);
-// 		  });
-// 	    })
-// 	    .catch(error => console.error('Error fetching patient data:', error));
-//   };
-
-  document.getElementById("list").onclick = function() {
-	// Fetch patient data from the server
-	document.getElementById("patientList").innerHTML = "";
-	fetch('/patients')
-	    .then(response => response.json())
-	    .then(data => {
-		  // Iterate through the patient data and display it
-		  data.forEach(patient => {
-			const patientList = document.getElementById('patientList');
-			const patientItem = document.createElement('p');
-			patientItem.textContent = `id: ${patient.id}, Name: ${patient.name}, Address: ${patient.address}, Birth Date: ${patient.birth_date}`;
-			patientList.appendChild(patientItem);
-		  });
-	    })
-	    .catch(error => console.error('Error fetching patient data:', error));
-  };
- document.getElementById("add").onclick = function () {
-	// let formDiv = document.getElementById("add-form");
-	// document.getElementsByClassName("add-form").style.display = "flex";
-	document.querySelector(".add-form").style.display = "flex";
-	// document.querySelector(".add-form").addEventListener('submit', function () {
-	// 	let name = document.getElementById("name").value;
-	// 	let address = document.getElementById("address").value;
-	// 	let birth_date = document.getElementById("birth_date").value;
-	// 	let ph_number = document.getElementById("ph_number").value;
-	// 	let number_of_days = document.getElementById("number_of_days").value;
-	// 	let room_id = document.getElementById("room_id").value;
-	// 	document.querySelector(".add-form").style.display = "none";
-	//  });
+document.getElementById("list").onclick = function() {
+	console.log(window.location.pathname)
+	if (window.location.pathname === '/index.html')
+	{
+		// Fetch patient data from the server
+		if (window.getComputedStyle(document.getElementById("patientList"), null).display === 'none') {
+			document.getElementById("patientList").style.display = 'block';
+			document.querySelector(".add-form").style.display = "none";
+			document.querySelector(".update-form").style.display = "none";
+			document.querySelector(".remove-form").style.display = "none";
+		}
+		else if (window.getComputedStyle(document.getElementById("patientList"), null).display === 'block') {
+			document.getElementById("patientList").style.display = 'none';
+		}
+		// console.log(document.getElementById("patientList").style.display == "block");
+		document.getElementById("patientList").innerHTML = "";
+		fetch('/patients')
+		.then(response => response.json())
+		.then(data => {
+			// Iterate through the patient data and display it
+			data.forEach(patient => {
+				const patientList = document.getElementById('patientList');
+				const patientItem = document.createElement('p');
+				patientItem.textContent = `id: ${patient.id}, Name: ${patient.name}, Address: ${patient.address}, Birth Date: ${patient.birth_date}`;
+				patientList.appendChild(patientItem);
+			});
+		})
+		.catch(error => console.error('Error fetching patient data:', error));
+	}
+};
+document.getElementById("add").onclick = function () {
+	if (document.querySelector(".add-form").style.display === "none") {
+		document.querySelector(".add-form").style.display = "flex";
+		document.querySelector(".update-form").style.display = "none";
+		document.getElementById("patientList").style.display = 'none';
+		document.querySelector(".remove-form").style.display = "none";
+	}
+	else if (document.querySelector(".add-form").style.display = "flex") {
+		document.querySelector(".add-form").style.display = "none";
+	}
 	document.getElementById("input-submit-add").onclick = function () {
 		let name = document.getElementById("name").value;
 		let address = document.getElementById("address").value;
@@ -54,6 +48,63 @@
 		
 		// Create a data object to send to the server
 		const data = {
+			name: name,
+		    address: address,
+		    birth_date: birth_date,
+		    ph_number: ph_number,
+		    number_of_days: number_of_days,
+		    room_id: room_id
+		};
+		
+		// Send a POST request to the server
+		fetch('/patient-add', {
+			method: 'POST',
+		    headers: {
+				'Content-Type': 'application/json'
+		    },
+		    body: JSON.stringify(data)
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+		    }
+		    return response.json();
+		})
+		.then(data => {
+		    console.log('Data sent successfully:', data);
+		    // Optionally, handle the response from the server here
+		})
+		.catch(error => {
+		    console.error('Error sending data to server:', error);
+		});
+		
+		// Hide the form after submitting
+		document.querySelector(".add-form").style.display = "none";
+	};
+	  
+};
+document.getElementById("edit").onclick = function () {
+	if (document.querySelector(".update-form").style.display === "none") {
+		document.querySelector(".update-form").style.display = "flex";
+		document.querySelector(".add-form").style.display = "none";
+		document.getElementById("patientList").style.display = 'none';
+		document.querySelector(".remove-form").style.display = "none";
+	}
+	else if (document.querySelector(".update-form").style.display = "flex") {
+		document.querySelector(".update-form").style.display = "none";
+	}
+	document.getElementById("input-submit-edit").onclick = function () {
+		let id = document.getElementById("id-edit").value;
+		let name = document.getElementById("name-edit").value;
+		let address = document.getElementById("address-edit").value;
+		let birth_date = document.getElementById("birth_date-edit").value;
+		let ph_number = document.getElementById("ph_number-edit").value;
+		let number_of_days = document.getElementById("number_of_days-edit").value;
+		let room_id = document.getElementById("room_id-edit").value;
+		
+		// Create a data object to send to the server
+		const data = {
+			id: id,
 		    name: name,
 		    address: address,
 		    birth_date: birth_date,
@@ -63,7 +114,7 @@
 		};
 	  
 		// Send a POST request to the server
-		fetch('/patient-add', {
+		fetch('/patient-edit', {
 		    method: 'POST',
 		    headers: {
 			  'Content-Type': 'application/json'
@@ -85,7 +136,53 @@
 		});
 	  
 		// Hide the form after submitting
+		document.querySelector(".update-form").style.display = "none";
+	  };
+	  
+};
+
+document.getElementById("remove").onclick = function () {
+	if (document.querySelector(".remove-form").style.display === "none") {
+		document.querySelector(".remove-form").style.display = "flex";
+		document.querySelector(".update-form").style.display = "none";
 		document.querySelector(".add-form").style.display = "none";
+		document.getElementById("patientList").style.display = 'none';
+	}
+	else if (document.querySelector(".remove-form").style.display = "flex") {
+		document.querySelector(".remove-form").style.display = "none";
+	}
+	document.getElementById("input-submit-remove").onclick = function () {
+		let id = document.getElementById("id-remove").value;
+		
+		// Create a data object to send to the server
+		const data = {
+			id: id,
+		};
+	  
+		// Send a POST request to the server
+		fetch('/patient-remove', {
+		    method: 'POST',
+		    headers: {
+			  'Content-Type': 'application/json'
+		    },
+		    body: JSON.stringify(data)
+		})
+		.then(response => {
+		    if (!response.ok) {
+			  throw new Error('Network response was not ok');
+		    }
+		    return response.json();
+		})
+		.then(data => {
+		    console.log('Data sent successfully:', data);
+		    // Optionally, handle the response from the server here
+		})
+		.catch(error => {
+		    console.error('Error sending data to server:', error);
+		});
+	  
+		// Hide the form after submitting
+		document.querySelector(".remove-form").style.display = "none";
 	  };
 	  
 };
--- interface/server.js
+++ interface/server.js
@@ -7,7 +7,8 @@ const port = 3000;
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    // port: 3306,
+    port: 3307,
     user: 'root',
     // password: 'root',
     database: 'hospital'
@@ -41,6 +42,47 @@ app.post('/patient-add', (req, res) => {
         }
     });
 });
+
+app.post('/patient-edit', (req, res) => {
+    // Extract data from the request body
+    const { id, name, address, birth_date, ph_number, number_of_days, room_id } = req.body;
+
+    // Construct SQL query to insert data into the database
+    const sql = `UPDATE patient SET name = ?, address = ?, birth_date = ?, ph_number = ?, number_of_days = ?, room_id = ? WHERE id = ?;`;
+    const values = [name, address, birth_date, ph_number, number_of_days, room_id, id];
+
+    // Execute the SQL query
+    connection.query(sql, values, (error, results) => {
+        if (error) {
+            console.error('Error editing patient data:', error);
+            res.status(500).json({ message: 'Error editing patient to the database' });
+        } else {
+            console.log('Patient edited successfully:', results);
+            res.status(200).json({ message: 'Patient edited successfully' });
+        }
+    });
+});
+
+app.post('/patient-remove', (req, res) => {
+    // Extract data from the request body
+    const { id } = req.body;
+
+    // Construct SQL query to insert data into the database
+    const sql = `DELETE FROM patient WHERE id = ?`;
+    const values = [id];
+
+    // Execute the SQL query
+    connection.query(sql, values, (error, results) => {
+        if (error) {
+            console.error('Error deleting patient data:', error);
+            res.status(500).json({ message: 'Error deleting patient to the database' });
+        } else {
+            console.log('Patient deleted successfully:', results);
+            res.status(200).json({ message: 'Patient deleted successfully' });
+        }
+    });
+});
+
 // Serve static files
 app.use(express.static('public'));
 
